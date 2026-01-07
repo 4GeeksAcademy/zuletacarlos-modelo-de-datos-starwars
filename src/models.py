810ff8f9889bb0c60f83fd09da14de40cdc25c81
zuletacarlos <carlosluiszuleta@yahoo.es>
2026-01-07 22:13:49 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -18,7 +18,6 @@ class User(db.Model):
             "email": self.email,
         }
 
-
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
